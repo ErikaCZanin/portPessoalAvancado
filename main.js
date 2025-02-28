47c0a526d3b382fff9cc44397289ef0279e0e6c5
ErikaZanin <erikaczbu@gmail.com>
2025-02-28 12:12:23 -0300

    Atualizando carregamento timeline

---
+++ main.js
@@ -0,0 +1,24 @@
+setTimeout(function(){
+    document.querySelector('.card1').style.display = 'block';
+    document.querySelector('#card1').style.display = 'block';
+}, 1000);
+
+setTimeout(function(){
+    document.querySelector('.card2').style.display = 'block';
+    document.querySelector('#card2').style.display = 'block';
+}, 2000);
+
+setTimeout(function(){
+    document.querySelector('.card3').style.display = 'block';
+    document.querySelector('#card3').style.display = 'block';
+}, 3000);
+
+setTimeout(function(){
+    document.querySelector('.card4').style.display = 'block';
+    document.querySelector('#card4').style.display = 'block';
+}, 4000);
+
+setTimeout(function(){
+    document.querySelector('.card5').style.display = 'block';
+    document.querySelector('#card5').style.display = 'block';
+}, 5000);
