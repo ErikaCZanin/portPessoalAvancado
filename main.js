6874e550bcd3f4f2ac37a35c33556c3255272d39
ErikaZanin <erikaczbu@gmail.com>
2025-03-11 11:18:09 -0300

    Atualizando layout

--- main.js
+++ main.js
@@ -22,4 +22,68 @@ window.addEventListener('scroll', function() {
     showCard(document.querySelector('#card4'), 2000);
     showCard(document.querySelector('.card5'), 2500);
     showCard(document.querySelector('#card5'), 2500);
+})
+
+// Função para aplicar o scale aos elementos
+function aplicarTransformacao(ids, scaleValue, colorValue) {
+    ids.forEach(id => {
+        const element = document.getElementById(id);
+        element.style.transform = `scale(${scaleValue})`;
+        element.style.color = colorValue; 
+    });
+}
+
+// Evento para o #inicio
+document.getElementById("inicio").addEventListener("mouseenter", function() {
+    this.style.transform = "scale(1.5)";
+    this.style.color = "white";
+    aplicarTransformacao(["desempenho", "projetos", "contato"], "0.8");
 });
+
+document.getElementById("inicio").addEventListener("mouseleave", function() {
+    this.style.transform = "scale(1)";
+    this.style.color = "#e4e7f3";
+    aplicarTransformacao(["desempenho", "projetos", "contato"], "1");
+});
+
+// Evento para o #desempenho
+document.getElementById("desempenho").addEventListener("mouseenter", function() {
+    this.style.transform = "scale(1.5)";
+    this.style.color = "white";
+    aplicarTransformacao(["inicio", "projetos", "contato"], "0.8");
+});
+
+document.getElementById("desempenho").addEventListener("mouseleave", function() {
+    this.style.transform = "scale(1)";
+    this.style.color = "#e4e7f3";
+    aplicarTransformacao(["inicio", "projetos", "contato"], "1");
+});
+
+
+// Evento para o #projetos
+document.getElementById("projetos").addEventListener("mouseenter", function() {
+    this.style.transform = "scale(1.5)";
+    this.style.color = "white";
+    aplicarTransformacao(["inicio", "desempenho", "contato"], "0.8");
+});
+
+document.getElementById("projetos").addEventListener("mouseleave", function() {
+    this.style.transform = "scale(1)";
+    this.style.color = "#e4e7f3";
+    aplicarTransformacao(["inicio", "desempenho", "contato"], "1");
+});
+
+// Evento para o #projetos
+document.getElementById("contato").addEventListener("mouseenter", function() {
+    this.style.transform = "scale(1.5)";
+    this.style.color = "white";
+    aplicarTransformacao(["inicio", "desempenho", "projetos"], "0.8");
+});
+
+document.getElementById("contato").addEventListener("mouseleave", function() {
+    this.style.transform = "scale(1)";
+    this.style.color = "#e4e7f3";
+    aplicarTransformacao(["inicio", "desempenho", "projetos"], "1");
+});
+
+;
